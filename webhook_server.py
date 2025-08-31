389bc828710ddc57b1f301a29a4be7218de22ba4
Angie M;arlen Martinez Montero <amarlen.montero@gmail.com>
2025-08-30 20:58:42 -0400

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -17,4 +17,4 @@ def recibir_pago():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=10000)
